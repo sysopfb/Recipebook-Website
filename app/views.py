90c7f0bb4e6ec8180c070f661971a59d612e8b7e
Jason R <sysopfb@gmail.com>
2013-06-22 13:15:29 -0500

    Migrated to MethodView for flask view

--- app/views.py
+++ app/views.py
@@ -1,30 +1,55 @@
 from flask import render_template, request, jsonify
 
+from flask.views import MethodView
+
+from sqlalchemy import func
+
+import json
+
 from app import app, db
 
-from models import User, Recipe, Post
+from models import Recipe
 
 
-@app.route("/recipes")
-def recipes():
-    recipes = db.session.query(Recipe).all()
+class RecipeAPI(MethodView):
+
+    def get(self):
+        searchstr = request.args.get('data')
+        if searchstr is None:
+            recipes = db.session.query(Recipe).all()
+        else:
+            recipes = db.session.query(Recipe).filter(func.substr(func.lower(Recipe.name), 1, len(searchstr)) == func.lower(searchstr)).all()
+
+        return jsonify(items=[recipe.to_json() for recipe in recipes])
     
-    return jsonify(items=[recipe.to_json() for recipe in recipes])
+    #put means a record was updated and sent
+    def put(self):
+        pass
 
+    #post means a new record was sent
+    def post(self):
+        data = json.loads(request.data)
+        recipe = Recipe()
+        recipe.name = data["name"]
+        recipe.ingredients = data["ingredients"]
+        recipe.instructions = data["instructions"]
+        recipe.author = data["author"]
+        db.session.add(recipe)
+        try:
+            db.session.commit()
+        except Exception,e:
+            print "error adding new record"
+            db.session.rollback()
+        return ""
+
+
+app.add_url_rule('/recipes', view_func=RecipeAPI.as_view('recipes'))
+'''
 @app.route("/users")
 def users():
-    #recs = {}
-    #for key, value in db.session.query(User).all():
-    #    users[key] = value
     users = db.session.query(User).all()
-    #for user in users:
-        #print user.to_json()
-    #print users
-    #oblist = [user.to_json() for user in users]
-    #print oblist
-    #print jsonify(oblist)
     return jsonify(items=[user.to_json() for user in users])
-
+'''
 @app.route("/")
 def index():
     return render_template('index.html')

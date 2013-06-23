c0e818a31d364416aec340628a58bc68af8e1be6
Jason R <sysopfb@gmail.com>
2013-06-22 21:53:00 -0500

    Changed BackBone views
    Changed from single view to multiple views for code clarity, also
    updated templates accordingly

--- app/static/views.js
+++ app/static/views.js
@@ -1,4 +1,4 @@
- var View = Backbone.View.extend({
+ var NavView = Backbone.View.extend({
     el: '#navigation',
     events: {
         'click #AllButton': 'getAll',
@@ -10,7 +10,7 @@
         recipes = new RecipeCollection();
         searchlist = new RecipeCollection();
     },
-    
+    /*
     getAll: function(event) {
         var rtemplate = _.template($('#recipe-template').html());
         //var users = new UsersCollection();
@@ -22,19 +22,23 @@
             });
         });
     },
-
+*/
+    getAll: function(even) {
+        recipes.fetch().done(function() {
+            var view = new RecipeView({collection: recipes});
+            view.render();
+        });
+    },
+    
     searchFunc : function(event) {
         var name = $('#sname');
-        var rtemplate = _.template($('#recipe-template').html());
         $('#dialog-search').dialog({
             modal: 'true',
             buttons: {
                 "Search": function() {
                     searchlist.fetch({data: $.param({'data': name.val()})}).done(function () {
-                        $('#recipe-list').html('');
-                        searchlist.forEach(function(model) {
-                            $('#recipe-list').append(rtemplate(model.toJSON()));
-                        });
+                        var view = new RecipeView({collection: searchlist});
+                        view.render();
                     });
                     $(this).dialog("close");
                 },
@@ -64,9 +68,10 @@
             buttons: {
                 "Create": function() {
                     var lines = ringredients.val().replace(/\r\n/g, "\n").split("\n");
+                    var lines2 = rinstructions.val().replace(/\r\n/g, "\n").split("\n");
                     recipes.create({name: rname.val(),
                                     ingredients: lines,
-                                    instructions: rinstructions.val(),
+                                    instructions: lines2,
                                     author: rauthor.val()
                                     });
                 },
@@ -79,6 +84,64 @@
                     $(this).dialog("close");
                 }
             }
+        });
+    }
+});
+
+RecipeView = Backbone.View.extend({
+    el: "#recipe-list",
+    tagName: "ul",
+    events: {
+        "click li": "clicked"
+    },
+
+    clicked: function(e){
+        e.preventDefault();
+        var r_id = $(e.currentTarget).data("id");
+        var item = this.collection.get(r_id);
+        //var name = item.get("name");
+        var rname = $('#name').val(item.get("name"));
+        ilist = item.get("ingredients");
+        instruct_list = item.get("instructions");
+        var ringredients = $('textarea#ingredients').val(ilist.join("\n"));
+        var rinstructions = $('textarea#instructions').val(instruct_list.join("\n"));
+        var rauthor = $('#author').val(item.get("author"));
+        $('#dialog-create').dialog({
+            modal: 'true',
+            buttons: {
+                "Update": function() {
+                    var lines = ringredients.val().replace(/\r\n/g, "\n").split("\n");
+                    var lines2 = rinstructions.val().replace(/\r\n/g, "\n").split("\n");
+                    item.save({id: r_id,
+                               name: rname.val(),
+                               ingredients: lines,
+                               instructions: lines2,
+                               author: rauthor.val()
+                               });
+                },
+                "Delete": function() {
+                    item.destroy();
+                },
+                Clear: function() {
+                    rname.val("");
+                    ringredients.val("");
+                    rinstructions.val("");
+                },
+                Cancel: function() {
+                    $(this).dialog("close");
+                }
+            }
+        });
+        
+    },
+
+     render: function(){
+        var rtemplate = _.template($('#recipe-template').html());
+        var el = $(this.el);
+        el.html('');
+        this.collection.each(function(model){
+            var html = rtemplate(model.toJSON());
+            el.append(html);
         });
     }
 });
